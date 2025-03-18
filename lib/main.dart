1037e86fb3ef9e3456be239655e48ce2c56c8b22
Matt <matthew_gates@hotmail.co.uk>
2025-03-18 00:04:29 +0000

    web build

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class GlyphOMatic extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Glyph',
       theme: ThemeData(
         // This is the theme of your application.
         //
